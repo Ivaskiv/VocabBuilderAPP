f7a965013a6f96c9b6efad55843a8027520e88d7
Ivaskiv <viraivaskiv@gmail.com>
2024-08-25 14:03:12 +0300

    apd css

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: true,
+    outDir: 'dist',
   },
   resolve: {
     alias: {
