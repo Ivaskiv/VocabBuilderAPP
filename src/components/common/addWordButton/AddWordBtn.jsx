ea1d196727f16902b84dd083c178431b885a1a64
Ivaskiv <viraivaskiv@gmail.com>
2024-09-13 21:32:21 +0200

    improved modals

--- src/components/common/addWordButton/AddWordBtn.jsx
+++ src/components/common/addWordButton/AddWordBtn.jsx
@@ -1,9 +1,22 @@
+import { useContext } from 'react';
 import styles from './styles.module.css';
 import { FaPlus } from 'react-icons/fa6';
+import ModalProvider, { ModalContext } from '../../modals/ModalProvider';
+import WordModal from '../../modals/WordModal';
 
 const AddWordBtn = () => {
+  const { openModal } = useContext(ModalContext);
+
+  const handleOpenModal = () => {
+    openModal(
+      <ModalProvider>
+        <WordModal mode="add" />
+      </ModalProvider>
+    );
+  };
+
   return (
-    <button className={styles.add_word_btn}>
+    <button className={styles.add_word_btn} onClick={handleOpenModal}>
       Add word <FaPlus className={styles.icon_add} />
     </button>
   );
