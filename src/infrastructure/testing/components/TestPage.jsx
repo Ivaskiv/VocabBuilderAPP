945d7f0ce555e6d5010f5de2b71adfd0f7fd4369
Ivaskiv <viraivaskiv@gmail.com>
2024-09-21 14:08:03 +0200

    test 4

--- src/infrastructure/testing/components/TestPage.jsx
+++ src/infrastructure/testing/components/TestPage.jsx
@@ -1,12 +1,12 @@
 import BallMover from '../tasks/features/task1/BallMover';
-import DispalyRundomValue from '../tasks/features/task2/DispalyRundomValue';
+import Task2 from '../tasks/features/task2/Task2';
 import styles from './styles.module.css';
 
 export default function TestPage() {
   return (
     <div className={styles.testing_container}>
       <BallMover />
-      <DispalyRundomValue />
+      <Task2 />
     </div>
   );
 }
