79672cbb7db0bfa3fa4bf666ff98c9e78d312615
Ivaskiv <viraivaskiv@gmail.com>
2024-09-26 11:29:01 +0200

    trim

--- src/infrastructure/utils/index.js
+++ src/infrastructure/utils/index.js
@@ -1,1 +1,12 @@
-export const trimObjStrValues = () => {};
+//trimObjStrValues.jsx
+export const trimObjStrValues = (obj, keys) => {
+  const keysArray = keys || Object.keys(obj);
+
+  return keysArray.reduce((trimmedObj, key) => {
+    if (key in obj) {
+      trimmedObj[key] = typeof obj[key] === 'string' ? obj[key].trim() : obj[key];
+    }
+
+    return trimmedObj;
+  });
+};
